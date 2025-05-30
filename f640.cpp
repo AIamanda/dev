3f4db49db29a9c725c551331739aff907d205a71
AIamanda <129344258+AIamanda@users.noreply.github.com>
2025-05-30 10:44:02 +0000

    new file:   a.out 	modified:   f640.cpp

--- f640.cpp
+++ f640.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 stack<char> opr;
 stack<int> num;
-
+stack<int> track_opr;
+stack<int> track_num;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     string s;
+    bool sat;
     getline(cin, s);
     int n = s.length();
     for(int i = 0; i < n; i++) {
@@ -15,20 +17,30 @@ int main() {
             string num_str = "";
             while(i < n && isdigit(s[i])) num_str += s[i++];
             num.push(stoi(num_str));
+            track_num.push(i);
         }
-        if(s[i] == ' ') continue;
-        if(s[i] == 'h') opr.push('h');
-        if(s[i] == 'g') opr.push('g');
-        if(s[i] == 'f') opr.push('f');
-        if(!num.empty()) {
+        if(s[i] == 'h') {
+            opr.push('h');
+            track_opr.push(i);
+        }else if(s[i] == 'g') {
+            opr.push('g');
+            track_opr.push(i);
+        }else if(s[i] == 'f') {
+            opr.push('f');
+            track_opr.push(i);
+        }
+        sat = false;
+        while(!sat && !num.empty() && !opr.empty()) {
             if(opr.top() == 'f') {
-                opr.pop();
-                int x = num.top()*2-3;
-                num.pop();
-                num.push(x);
+                if(track_opr.top() < track_num.top()) {
+                    opr.pop();
+                    int x = num.top()*2-3;
+                    num.pop();
+                    num.push(x);
+                }
             }
-            if(opr.top() == 'g') {
         }
     }
+    cout << num.top() << endl;
     return 0;
 }
