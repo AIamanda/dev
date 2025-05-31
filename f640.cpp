11226088e76bb7bcc294debe7b923396b56888c7
AIamanda <129344258+AIamanda@users.noreply.github.com>
2025-05-31 09:04:44 +0000

    deleted:    .gitignore 	deleted:    LICENSE 	deleted:    a.out 	modified:   f640.cpp 	deleted:    requirements.txt

--- f640.cpp
+++ f640.cpp
@@ -1,42 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 stack<char> opr;
-stack<int> num;
+stack<long long int> num;
 stack<int> track_opr;
-stack<int> track_num;
+stack<long long int> track_num;
 
+void popr(char x, int i) {
+    opr.push(x);
+    track_opr.push(i);
+}
+void pnum(int x, int i) {
+    num.push(x);
+    track_num.push(i);
+}
+void dopr() {
+    opr.pop();
+    track_opr.pop();
+}
+void dnum() {
+    num.pop();
+    track_num.pop();
+}
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
     string s;
     bool sat;
     getline(cin, s);
-    int n = s.length();
+    int n = s.length(), temp[3];
     for(int i = 0; i < n; i++) {
-        if(isdigit(s[i])) {
+        if(isdigit(s[i]) || s[i] == '-') {
             string num_str = "";
-            while(i < n && isdigit(s[i])) num_str += s[i++];
-            num.push(stoi(num_str));
-            track_num.push(i);
+            while(i < n && (isdigit(s[i]) || s[i] == '-')) num_str += s[i++];
+            if(i == n) i--;
+            pnum(stoi(num_str), i);
         }
-        if(s[i] == 'h') {
-            opr.push('h');
-            track_opr.push(i);
-        }else if(s[i] == 'g') {
-            opr.push('g');
-            track_opr.push(i);
-        }else if(s[i] == 'f') {
-            opr.push('f');
-            track_opr.push(i);
-        }
+        if(s[i] == 'h') popr('h', i);
+        if(s[i] == 'g') popr('g', i);
+        if(s[i] == 'f') popr('f', i);
         sat = false;
         while(!sat && !num.empty() && !opr.empty()) {
             if(opr.top() == 'f') {
                 if(track_opr.top() < track_num.top()) {
-                    opr.pop();
+                    dopr();
                     int x = num.top()*2-3;
-                    num.pop();
-                    num.push(x);
+                    dnum();
+                    pnum(x, i);
+                }else sat = true;
+            }
+            else if (opr.top() == 'g') {
+                if(track_opr.top() < track_num.top() && !(track_num.size() == 1)) {
+                    int tmp = track_num.top();
+                    track_num.pop();
+                    if(track_opr.top() < track_num.top()) {
+                        track_num.push(tmp);
+                        dopr();
+                        int x1 = num.top();
+                        dnum();
+                        int x2 = num.top();
+                        dnum();
+                        pnum((x2*2)+x1-7, i);
+                    }else {
+                        track_num.push(tmp);
+                        sat = true;
+                    }
+                }else sat = true;
+            }
+            else if(opr.top() == 'h') {
+                if(num.size() < 3) sat = true;
+                if(!sat && track_opr.top() < track_num.top()) {
+                    temp[0] = track_num.top();
+                    track_num.pop();
+                    temp[1] = track_num.top();
+                    track_num.pop();
+                    temp[2] = track_num.top();
+                    track_num.pop();
+                    if(track_opr.top() > temp[2]) sat = true;
+                    track_num.push(temp[2]);
+                    track_num.push(temp[1]);
+                    track_num.push(temp[0]);
+                }else sat = true;
+                if(!sat) {
+                    dopr();
+                    int x1 = num.top();
+                    dnum();
+                    int x2 = num.top();
+                    dnum();
+                    int x3 = num.top();
+                    dnum();
+                    pnum((x3*3)-(x2*2)+x1, i);
                 }
             }
         }
