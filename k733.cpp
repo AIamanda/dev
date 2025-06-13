b02053dfa9db84a3acf31ca9a6c4072e0b8b48a5
AIamanda <129344258+AIamanda@users.noreply.github.com>
2025-06-13 08:29:51 +0000

    new file:   .vscode/settings.json 	modified:   a.out 	new file:   hello-world.ipynb 	modified:   k733.cpp
    	deleted:    .vscode/settings.json
    	deleted:    a.out

--- k733.cpp
+++ k733.cpp
@@ -1,22 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    string s;
-    getline(cin, s);
-    int n = s.length();
-    int at;
-    for(int i = 0; i < n; i++) {
-        if(s[i] == 'T') {
-            string num_str = "";
-            i++;
-            while(isdigit(s[i])) {
-                num_str += s[i];
-                i++;
-            }
-            at = stoi(num_str);
-            i = n;
-        }
+int magnet(string &s, string &copy, int idx, int last_one) {
+    if(idx >= copy.length()) return 0;
+    if(copy[idx] == 'T') {
+        int tmp = copy[++idx]*10+copy[++idx];
+        if(!last_one) return magnet(s, copy, (idx+1), tmp);
+        return abs((tmp-last_one)) + magnet(s, copy, (idx+1), tmp);
     }
-    string 
+    else if(copy[idx] == 'L') {
+        copy[idx+1]--;
+        return magnet(s, copy, (idx+2), last_one);
+    }
+    else if(copy[idx] == 'E') {
+        int temp = idx, val = 0;
+        for(; temp >= 0; --temp) {
+            if(copy[temp] == 'E') val ++;
+            else if(copy[temp] == 'L') val--;
+            if(val == 0) break;
+        }
+        if(copy[temp+1] == '0') {
+            copy[temp+1] = s[temp+1];
+            return magnet(s, copy, (idx+1), last_one);
+        }
+        else {
+            copy[temp+1]--;
+            idx = temp+2;
+            return magnet(s, copy, idx, last_one);
+        }
+        return 0;
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    cin >> s;
+    string copy = s;
+    cout << magnet(s, copy, 0, 0) << endl;
+    return 0;
 }
